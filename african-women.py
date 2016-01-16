d1e1c961235e021c5c35b13e7bee5957212dec58
emijrp <emijrp@gmail.com>
2016-01-16 12:03:05 +0100

    more columns

--- african-women.py
+++ african-women.py
@@ -22,22 +22,38 @@ import urllib
 
 def main():
     limit = 0
-    if len(sys.argv) == 2:
-        limit = int(sys.argv[1])
+    country = ''
+    localwiki = ''
+    if len(sys.argv) >= 2:
+        localwiki = sys.argv[1]
+    if len(sys.argv) >= 3:
+        country = sys.argv[2]
+    if len(sys.argv) >= 4:
+        limit = int(sys.argv[3])
+    if len(sys.argv) <= 1:
+        print('Use: python script.py LOCALWIKI COUNTRYNAME LIMIT')
+        sys.exit()
     
     site = pywikibot.Site('en', 'wikipedia')
     repo = site.data_repository()
-    
     people = []
     
-    url = 'https://tools.wmflabs.org/autolist/index.php?language=en&project=wikipedia&category=&depth=12&wdq=claim[31%3A6256]%20claim[30%3A15]&pagepile=&statementlist=&run=Run&mode_manual=or&mode_cat=or&mode_wdq=not&mode_find=or&chunk_size=10000&download=1'
-    f = urllib.request.urlopen(url)
-    countryids = f.read().strip().splitlines()
-    print('Loaded',len(countryids),'countries')
+    countryids = []
+    if country.lower() == 'africa':
+        url = 'https://tools.wmflabs.org/autolist/index.php?language=en&project=wikipedia&category=&depth=12&wdq=claim[31%3A6256]%20claim[30%3A15]&pagepile=&statementlist=&run=Run&mode_manual=or&mode_cat=or&mode_wdq=not&mode_find=or&chunk_size=1000&download=1'
+        f = urllib.request.urlopen(url)
+        countryids = f.read().strip().splitlines()
+        countryids.remove(b'Q142')
+        countryids.remove(b'Q29')
+        print('Loaded',len(countryids),'countries')
+    else:
+        page = pywikibot.Page(site, country)
+        item = pywikibot.ItemPage.fromPage(page)
+        countryids = [item.getID().encode()]
+    print('Analysing',countryids)
     
     salir = False
-    skip = [b'Q142', b'Q29']
-    localwiki = 'fr'
+    skip = []
     for countryid in countryids:
         if salir:
             break
@@ -53,24 +69,28 @@ def main():
             print('Skiping...')
             continue
         
-        url2 = 'https://tools.wmflabs.org/autolist/index.php?language=en&project=wikipedia&category=&depth=12&wdq=claim[21%%3A6581072]%%20claim[27%%3A%s]&pagepile=&statementlist=&run=Run&mode_manual=or&mode_cat=or&mode_wdq=not&mode_find=or&chunk_size=10000&download=1' % (countryid.decode("utf-8")[1:])
+        url2 = 'https://tools.wmflabs.org/autolist/index.php?language=en&project=wikipedia&category=&depth=12&wdq=claim[21%%3A6581072]%%20claim[27%%3A%s]&pagepile=&statementlist=&run=Run&mode_manual=or&mode_cat=or&mode_wdq=not&mode_find=or&chunk_size=%s&download=1' % (countryid.decode("utf-8")[1:], limit*2)
         f = urllib.request.urlopen(url2)
         personids = f.read().strip().splitlines()
         
         for personid in personids:
-            person = {'name': '-', 'birth': '', 'death': '', 'occupation': '', 'image': ''}
+            person = {'q': '', 'name': '-', 'birth': '', 'death': '', 'occupation': [], 'image': '', 'commons': ''}
             personitem = pywikibot.ItemPage(repo, personid.decode("utf-8"))
             personitem.get()
             
-            if localwiki in personitem.labels:
-                #print('It exists in this Wikipedia, skiping')
+            if localwiki+'wiki' in personitem.sitelinks.keys():
+                print('\n',personid,'exists in',localwiki,'wiki, skiping',personitem.sitelinks)
                 continue
             
-            print(personitem.sitelinks)
             person['sitelinks'] = personitem.sitelinks
-            if not list(personitem.sitelinks.keys()):
+            if 'commonswiki' in person['sitelinks']:
+                person['commons'] = person['sitelinks']['commonswiki']
+                person['sitelinks'].pop('commonswiki')
+            if not list(person['sitelinks'].keys()):
                 continue
+            print('\n',person['sitelinks'])
             
+            person['q'] = personid
             person['name'] = personitem.sitelinks[list(personitem.sitelinks.keys())[0]]
             if personitem.claims:
                 if 'P18' in personitem.claims:
@@ -79,19 +99,20 @@ def main():
                         print('Image:', image.title())
                         person['image'] = image.title()
                 if 'P106' in personitem.claims:
-                    ocitem = personitem.claims['P106'][0].getTarget()
-                    try:
-                        ocitem.get()
-                        if localwiki in ocitem.labels:
-                            print('Occupation:', ocitem.labels[localwiki])
-                            person['occupation'] = ocitem.labels[localwiki]
-                        elif 'en' in ocitem.labels:
-                            print('Occupation:', ocitem.labels['en'])
-                            person['occupation'] = ocitem.labels['en']
-                        else:
+                    for p106 in personitem.claims['P106']:
+                        ocitem = p106.getTarget()
+                        try:
+                            ocitem.get()
+                            if localwiki in ocitem.labels:
+                                print('Occupation:', ocitem.labels[localwiki])
+                                person['occupation'].append(ocitem.labels[localwiki])
+                            elif 'en' in ocitem.labels:
+                                print('Occupation:', ocitem.labels['en'])
+                                person['occupation'].append(ocitem.labels['en'])
+                            else:
+                                pass
+                        except:
                             pass
-                    except:
-                        pass
                 if 'P569' in personitem.claims:
                     birth = personitem.claims['P569'][0].getTarget()
                     if birth:
@@ -114,10 +135,10 @@ def main():
     people.sort()
     output = '\n'*3
     output += '{| class="wikitable sortable"\n'
-    output += '! Name !! Occupation !! Birth !! Death !! Country !! Image\n'
+    output += '! Name !! Occupation !! Birth !! Death !! Country !! Image !! Iw\n'
     for person in people:
        interwiki = ', '.join(['[[:%s:%s|%s]]' % (k.split('wiki')[0], v, k.split('wiki')[0]) for k, v in person[2]["sitelinks"].items()])
-       output += '|-\n| [[%s]] <small>(%s)</small> || %s || %s || %s || [[%s]] || %s\n' % (person[1], interwiki, person[2]["occupation"] and person[2]["occupation"] or 'unknown', person[2]["birth"] and person[2]["birth"] or 'unknown', person[2]["death"] and person[2]["death"] or 'unknown', person[0], person[2]["image"] and '[[%s|80px]]' % person[2]["image"] or '-')
+       output += '|-\n| [[%s]] <small>(%s)</small> || %s || %s || %s || [[%s]] || %s%s || [[:d:%s|%s]]\n' % (person[1], interwiki, person[2]["occupation"] and ', '.join(person[2]["occupation"]) or 'unknown', person[2]["birth"] and person[2]["birth"] or 'unknown', person[2]["death"] and person[2]["death"] or 'unknown', person[0], person[2]["image"] and '[[%s|80px]]' % person[2]["image"] or '-', person[2]['commons'] and '<br/>[[:commons:%s|Commons]]' % person[2]['commons'], person[2]['q'].decode("utf-8"), len(person[2]['sitelinks']))
     output += '|}\n'
     
     print(output)
